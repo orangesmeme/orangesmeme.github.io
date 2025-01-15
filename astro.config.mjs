2a24be92ef60688c8a57c496992e529ee8930351
bitshiftmod <bitshiftmod@gmail.com>
2025-01-15 01:47:48 -0500

    added tailwind for styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://orangesmeme.github.io',
-	integrations: [
-		starlight({
-			title: 'Orange',
-			social: {
-				github: 'https://github.com/orangesmeme/orangesmeme.github.io',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Guide to Orange', slug: 'guides/orange' },
-						{ label: 'Community', slug: 'guides/community' },
-					],
-				},
-				{
-					label: 'Events',
-					autogenerate: { directory: 'events' },
-				},
-			],
-		}),
-	],
+  site: "https://orangesmeme.github.io",
+  integrations: [
+    starlight({
+      title: "Orange",
+      customCss: [
+        // Path to your Tailwind base styles:
+        "./src/tailwind.css",
+      ],
+      social: {
+        github: "https://github.com/orangesmeme/orangesmeme.github.io",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Guide to Orange", slug: "guides/orange" },
+            { label: "Community", slug: "guides/community" },
+          ],
+        },
+        {
+          label: "Events",
+          autogenerate: { directory: "events" },
+        },
+      ],
+    }),
+    tailwind({
+      // Disable the default base styles:
+      applyBaseStyles: false,
+    }),
+  ],
 });
